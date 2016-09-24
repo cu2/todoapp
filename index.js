c1533b76ab98035bb1965d09aeff4d23eb43c704
Andras Nemeth <cucu.szpem@gmail.com>
2016-09-24 15:06:31 +0200

    [db] switched to mongo

--- index.js
+++ index.js
@@ -2,116 +2,57 @@
 
 var express = require('express');
 var bodyparser = require('body-parser');
-var sqlite3 = require('sqlite3').verbose();
+var mongo = require('mongodb');
+var monk = require('monk');
 
 var app = express();
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
 
-var db = new sqlite3.Database('todo.db');
+var db = monk('localhost:27017/tododb');
+var collection = db.get('todo');
 
 
 app.use('/static', express.static(__dirname + '/static'));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 
 
-app.post('/todo/', function (req, res) {
-  db.run('INSERT INTO todo (text) VALUES(?)', req.body.text, function(err) {
-    if (err !== null) {
+app.post('/todo/', (req, res) => {
+  collection.insert({
+    "text": req.body.text,
+    "done": false,
+  }, (err, doc) => {
+    if (err) {
       console.log(err);
       res.status(500).json({
         error: err,
       });
     } else {
-      db.get('SELECT * FROM todo WHERE id = ?', this.lastID, function(err, row) {
-        if (err !== null) {
-          console.log(err);
-          res.status(500).json({
-            error: err,
-          });
-        } else {
-          res.json(row);
-        }
-      });
+      res.json(doc);
     }
   });
 });
 
-app.get('/todo/:id/?', function (req, res) {
-  db.get('SELECT * FROM todo WHERE id = ?', req.params.id, function(err, row) {
-    if (err !== null) {
+app.get('/todo/', (req, res) => {
+  // TODO: add filter based on req.query.done
+  collection.find({}, {}, (err, docs) => {
+    if (err) {
       console.log(err);
       res.status(500).json({
         error: err,
       });
     } else {
-      res.json(row);
-    }
-  });
-});
-
-app.put('/todo/:id/?', function (req, res) {
-  db.run('UPDATE todo SET text = ?, done = ? WHERE id = ?', req.body.text, req.body.done, req.params.id, function(err) {
-    if (err !== null) {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    } else {
-      db.get('SELECT * FROM todo WHERE id = ?', req.params.id, function(err, row) {
-        if (err !== null) {
-          console.log(err);
-          res.status(500).json({
-            error: err,
-          });
-        } else {
-          res.json(row);
-        }
-      });
-    }
-  });
-});
-
-app.delete('/todo/:id/?', function (req, res) {
-  db.run('DELETE FROM todo WHERE id = ?', req.params.id, function(err) {
-    if (err !== null) {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    } else {
-      res.json({
-        deleted: this.changes,
-      });
-    }
-  });
-});
-
-app.get('/todo/', function (req, res) {
-  let sqlCommand = 'SELECT * FROM todo';
-  let sqlParams = [];
-  if (req.query.done !== undefined) {
-    sqlCommand += ' WHERE done = ?';
-    sqlParams.push(req.query.done);
-  }
-  db.all(sqlCommand, sqlParams, function(err, rows) {
-    if (err !== null) {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    } else {
-      res.json(rows);
+      res.json(docs);
     }
   });
 });
 
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3000, () => {
+  console.log('Todo app listening on port 3000!');
 });
