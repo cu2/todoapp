abd3392f67c1ab9e563442bd055dec11d1ea4eaa
Andras Nemeth <cucu.szpem@gmail.com>
2016-09-24 10:53:47 +0200

    [frontend] create todo added

--- static/js/main.js
+++ static/js/main.js
@@ -9,6 +9,15 @@ var TodoApp = {
       callback();
     });
   },
+  createTodo: (todo, callback) => {
+    $.ajax({
+      type: 'POST',
+      url: '/todo/',
+      data: JSON.stringify(todo),
+      contentType: 'application/json',
+      success: callback,
+    });
+  },
   render: () => {
     let html = [];
     html.push('<ul>');
@@ -19,10 +28,29 @@ var TodoApp = {
       html.push('</li>');
     });
     html.push('</ul>');
-    $('#todos').html(html.join(''));
-  }
+    $('#todo_list').html(html.join(''));
+  },
+  init: () => {
+    let html = [];
+    html.push('<h1>Todos:</h1>');
+    html.push('<div id="todo_list"></div>');
+    html.push('<input type="text" id="new_todo" />');
+    $('#todoapp').html(html.join(''));
+    $('#new_todo').keyup((e) => {
+      if (e.keyCode === 13) {
+        TodoApp.createTodo({
+          text: $(e.target).val()
+        }, () => {
+          TodoApp.getTodos(TodoApp.render);
+        });
+        $(e.target).val('');
+      }
+    });
+    TodoApp.render();
+    $('#new_todo').focus();
+  },
 };
 
 $(() => {
-  TodoApp.getTodos(TodoApp.render);
+  TodoApp.getTodos(TodoApp.init);
 });
